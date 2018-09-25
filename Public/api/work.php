dde1b613c7f147d6da7ac6a6b118de191416283a
AJ Trittschuh <atrittsc@indiana.edu>
2018-09-25 13:49:56 -0400

    9/24 end of class (really) green dev

--- Public/api/work.php
+++ Public/api/work.php
@@ -1,20 +1,32 @@
-2
 <?php
-
 require '../../app/common.php';
+// Get the taskId from URL params
+$taskId = intval($_GET['taskId'] ?? 0);
+// Fetch the work from the db
+$work = Work::findByTaskId($taskId);
+// convert to json and print
+echo json_encode($work);
 
-$taskId = intval()$_GET['taskId'] ?? 0);
-
-if ($taskId < 1) {
-  throw new Exception('Invalid Task ID');
-}
 
 
-// 1. go to the databse and get all work associated with the $taskId
-$workArr = Work::getAllWorkByTask($taskId);
 
-// 2. convert to Json
-$json = json_encode($workArr);
 
-// 3. print
-echo $json
+
+
+// require '../../app/common.php';
+//
+// $taskId = intval()$_GET['taskId'] ?? 0);
+//
+// if ($taskId < 1) {
+//   throw new Exception('Invalid Task ID');
+// }
+//
+//
+// // 1. go to the databse and get all work associated with the $taskId
+// $workArr = Work::getAllWorkByTask($taskId);
+//
+// // 2. convert to Json
+// $json = json_encode($workArr);
+//
+// // 3. print
+// echo $json
