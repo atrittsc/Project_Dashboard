1fede7748bb8f881b16c09448183a96089aee9b8
AJ Trittschuh <atrittsc@indiana.edu>
2018-09-25 21:51:38 -0400

    catch up on Video 11

--- APP/common.php
+++ APP/common.php
@@ -1,7 +1,9 @@
 <?php
+
+chdir(__DIR__);
+set_include_path (__DIR__);
+
 require 'environment.php';
 // TODO: Require object files
 require 'models/Work.php';
 // change the working directory to this file //
-chdir(__DIR__);
-set_include_path (__DIR__);
